6a0be355d2ea2dc1e53965ec0f831fecb44c7ca7
masonmcbride <mcbridedmason@gmail.com>
2024-08-24 19:33:31 -0700

    problem 5

--- e005.py
+++ e005.py
@@ -1,23 +1,10 @@
 # Project euler problem 5
 # projecteuler.net/problem=5
 
+from math import lcm 
 
 def compute():
-    pass
+    return lcm(*range(1,21))
 
 if __name__ == '__main__':
     print(compute())
-
-# 1|n, 2|n, 3|n, ..., 20|n = min(n : Z) 
-
-# recall: a|b iff k * a = b
-"""
-so 1|n = k1 * 1 = N => n is trivially k1 in this case
-   2|n = k2 * 2 = N
-   3|n = k3 * 3 = N
-   4|n = k4 * 4 = N
-   5|n = k5 * 5 = N
-   ...
-   20|n = k20 * 20 = N
-
-"""
